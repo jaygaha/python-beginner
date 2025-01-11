0aa7c38f7e3e39664f75ce58bee758213e2bcaad
Jay Gaha <jaygaha@gmail.com>
2025-01-11 14:23:14 +0900

    day #15 control flow

---
+++ workspace/50_udacity/4_control_flow/quiz/1_conditional_statment_control_flow.py
@@ -0,0 +1,127 @@
+# Control Flow
+#
+# Conditional statements
+#
+# if
+# elif
+# else
+# if-else
+# if-elif-else
+
+#First Example - try changing the value of phone_balance
+phone_balance = 10
+bank_balance = 50
+
+if phone_balance < 10:
+    phone_balance += 10
+    bank_balance -= 10
+
+print(phone_balance)
+print(bank_balance)
+
+#Second Example - try changing the value of number
+
+number = 145
+if number % 2 == 0:
+    print("Number " + str(number) + " is even.")
+else:
+    print("Number " + str(number) + " is odd.")
+
+#Third Example - try to change the value of age
+age = 35
+
+# Here are the age limits for bus fares
+free_up_to_age = 4
+child_up_to_age = 18
+senior_from_age = 65
+
+# These lines determine the bus fare prices
+concession_ticket = 1.25
+adult_ticket = 2.50
+
+# Here is the logic for bus fare prices
+if age <= free_up_to_age:
+    ticket_price = 0
+elif age <= child_up_to_age:
+    ticket_price = concession_ticket
+elif age >= senior_from_age:
+    ticket_price = concession_ticket
+else:
+    ticket_price = adult_ticket
+
+message = "Somebody who is {} years old will pay ${} to ride the bus.".format(age, ticket_price)
+print(message)
+
+# Quiz
+points = 174  # use this input to make your submission
+
+# your code goes here
+result = ""
+if points <= 50:
+    result = "Congratulations! You won a wooden rabbit!"
+elif points <= 150:
+    result = "Oh dear, no prize this time."
+elif  points <= 180:
+    result = "Congratulations! You won a wafer-thin mint!"
+else:
+    result = "Congratulations! You won a penguin!"
+
+print(result)
+
+print()
+
+# Quiz: Guess My Number
+# '''
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+
+answer = 42
+guess = 42
+
+result = ""
+
+if guess < answer:
+    result = "Oops! Your guess is too low."
+elif guess > answer:
+    result = "Oops! Your guess is too high."
+elif guess == answer:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
+
+print()
+# Quiz Tax Purchase
+# '''
+# Depending on where an individual is from we need to tax them
+# appropriately.  The states of CA, MN, and
+# NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
+# Use this information to take the amount of a purchase and
+# the corresponding state to assure that they are taxed by the right
+# amount.
+# '''
+state = "CA" #Either CA, MN, or NY
+purchase_amount = 69.99 #amount of purchase
+
+if state == "CA":
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+elif state == "MN":
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+elif state == "NY":
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
+
+
