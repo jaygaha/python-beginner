867ec7b5e83544cca43b75785b7b9a843718515d
Jay Gaha <gaha@buzzreach.co.jp>
2025-01-31 11:08:11 +0900

    day #30 hanoe_tower

--- workspace/61_freecodecamp/8_algorithm_design/shortest_path.py
+++ workspace/61_freecodecamp/8_algorithm_design/shortest_path.py
@@ -1,10 +1,16 @@
 # Algorithm Design: Algorithms are step-by-step procedures that developers use to perform calculations and solve computational problems.
 # Shortest Path
 my_graph = {
+    # 'A': [('B', 3), ('D', 1)],
+    # 'B': [('A', 3), ('C', 4)],
+    # 'C': [('B', 4), ('D', 7)],
+    # 'D': [('A', 1), ('C', 7)]
+
+    # example2
     'A': [('B', 5), ('C', 3), ('E', 11)],
     'B': [('A', 5), ('C', 1), ('F', 2)],
     'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
-    'D': [('C',1 ), ('E', 9), ('F', 3)],
+    'D': [('C', 1), ('E', 9), ('F', 3)],
     'E': [('A', 11), ('C', 5), ('D', 9)],
     'F': [('B', 2), ('D', 3)]
 }
--- workspace/61_freecodecamp/9_recursion/hanoe_tower.py
+++ workspace/61_freecodecamp/9_recursion/hanoe_tower.py
@@ -1,56 +1,33 @@
-# Recursion: Recursion is a programming approach that allows you to solve complicated computational problems with just a little code.
-# In this project, you'll start with a loop-based approach to solving the tower of Hanoi mathematical puzzle. Then you'll learn how to implement a recursive solution.
+# To solve the puzzle with recursion, the first thing to do is break the original problem down into smaller sub-problems.
+# The final configuration with n disks piled up to the third rod in decreasing order can be obtained by moving:
+#     n - 1 disks from the source to the auxiliary rod
+#     the largest disk from the source to the target
+#     and then the n - 1 disks from the auxiliary rod to the target.
+# So, the first thing the move function should do is calling itself with n - 1 as the first argument. But if you try to do so without defining a base case, you will get a RecursionError.
+#   This happens because the function keeps calling itself indefinitely.
+# Before your comment and your print() call, add the recursive function call with n - 1 as the first argument and make sure the function body executes only when n is greater than zero.
+#   For now, leave the other arguments in the same order.
 
-# def hanoe_tower(n):
-#     if n == 1:
-#         return 1
-#     else:
-#         return n * hanoe_tower(n - 1)
+NUMBER_OF_DISKS = 5
+A = list(range(NUMBER_OF_DISKS, 0, -1))
+B = []
+C = []
 
-# print(hanoe_tower(4))
+def move(n, source, auxiliary, target):
+    if n <= 0:
+        return
 
-# create a function for recusion
+    # move n - 1 disks from source to auxiliary, so they are out of the way
+    move(n - 1, source, target, auxiliary)
 
-my_graph = {
-    # 'A': [('B', 3), ('D', 1)],
-    # 'B': [('A', 3), ('C', 4)],
-    # 'C': [('B', 4), ('D', 7)],
-    # 'D': [('A', 1), ('C', 7)]
+    # move the nth disk from source to target
+    target.append(source.pop())
 
-    # example2
-    'A': [('B', 5), ('C', 3), ('E', 11)],
-    'B': [('A', 5), ('C', 1), ('F', 2)],
-    'C': [('A', 3), ('B', 1), ('D', 1), ('E', 5)],
-    'D': [('C', 1), ('E', 9), ('F', 3)],
-    'E': [('A', 11), ('C', 5), ('D', 9)],
-    'F': [('B', 2), ('D', 3)]
-}
-def shortest_path(graph, start, target = ''):
-    unvisited = list(graph)
-    distances = {node: 0 if node == start else float('inf') for node in graph}
-    paths = {node: [] for node in graph}
-    paths[start].append(start)
+    # display our progress
+    print(A, B, C, '\n')
 
-    while unvisited:
-        current = min(unvisited, key=distances.get)
-        for node, distance in graph[current]:
-            if distance + distances[current] < distances[node]:
-                distances[node] = distance + distances[current]
-                if paths[node] and paths[node][-1] == node:
-                    paths[node] = paths[current][:]
-                else:
-                    paths[node].extend(paths[current])
-                paths[node].append(node)
-        unvisited.remove(current)
+    # move the n - 1 disks that we left on auxiliary onto target
+    move(n - 1,  auxiliary, source, target)
 
-    targets_to_print = [target] if target else graph
-    for node in targets_to_print:
-        if node == start:
-            continue
-        print(f'\n{start}-{node} distance: {distances[node]}\nPath: {" -> ".join(paths[node])}')
-
-    return distances, paths
-
-# shortest_path(my_graph, 'A')
-
-shortest_path(my_graph, 'A', 'F') # shortest path from A to F  ## A-F distance: 6 Path: A -> C -> B -> F
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, A, B, C)
