a37d9caedcd50d68a5c2276028decb621b8257c6
Jay Gaha <gaha@buzzreach.co.jp>
2025-01-31 11:08:11 +0900

    day #30 hanoe_tower

---
+++ workspace/61_freecodecamp/9_recursion/hanoe_tower_without.py
@@ -0,0 +1,61 @@
+# Recursion: Recursion is a programming approach that allows you to solve complicated computational problems with just a little code.
+# In this project, you'll start with a loop-based approach to solving the tower of Hanoi mathematical puzzle. Then you'll learn how to implement a recursive solution.
+
+# def hanoe_tower(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * hanoe_tower(n - 1)
+
+# print(hanoe_tower(4))
+
+# Without recursion
+NUMBER_OF_DISKS = 4
+number_of_moves = 2 ** NUMBER_OF_DISKS - 1
+rods = {
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
+    'B': [],
+    'C': []
+}
+
+def make_allowed_move(rod1, rod2):
+    forward = False
+    if not rods[rod2]:
+        forward = True
+    elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
+        forward = True
+    if forward:
+        print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
+        rods[rod2].append(rods[rod1].pop())
+    else:
+        print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
+        rods[rod1].append(rods[rod2].pop())
+
+    # display our progress
+    print(rods, '\n')
+
+def move(n, source, auxiliary, target):
+    # display starting configuration
+    print(rods, '\n')
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            if n % 2 != 0:
+                print(f'Move {i + 1} allowed between {source} and {target}')
+                make_allowed_move(source, target)
+            else:
+                print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+                make_allowed_move(source, auxiliary)
+        elif remainder == 2:
+            if n % 2 != 0:
+                print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+                make_allowed_move(source, auxiliary)
+            else:
+                print(f'Move {i + 1} allowed between {source} and {target}')
+                make_allowed_move(source, target)
+        elif remainder == 0:
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
+            make_allowed_move(auxiliary, target)
+
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
