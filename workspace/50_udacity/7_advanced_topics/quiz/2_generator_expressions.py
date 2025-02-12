a635b70a257d55f4927bb4afc0289b69581415e9
Jay Gaha <gaha@buzzreach.co.jp>
2025-02-12 14:46:06 +0900

    day #34 vector space

--- workspace/50_udacity/7_advanced_topics/quiz/2_generator_expressions.py
+++ workspace/50_udacity/7_advanced_topics/quiz/2_generator_expressions.py
@@ -11,6 +11,8 @@ print(sq_list) # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 sq_iterator = (x**2 for x in range(10)) # this produces a generator object that can be used to iterate over the squares of the numbers from 0 to 9
 
 print(sq_iterator) # <generator object <genexpr> at 0x7f8b0e3b0b50>
+print([sq_iterator])
 
 print(sq_iterator.__next__()) # 0
+print(sq_iterator.__next__()) # 1
 print(sq_iterator.__next__()) # 1
